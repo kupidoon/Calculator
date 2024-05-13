25393dbdad1e2fd89a0c36e61cbfe39495684f0d
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-05-13 17:12:29 +0200

    Перший коміт

---
+++ calculator.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void dodavanie(int pervoechislo, int vtoroechislo);
+void mnoshenie(int pervoechislo, int vtoroechislo);
+void delenie(int pervoechislo, int vtoroechislo);
+void vidnimana(int pervoechislo, int vtoroechislo);
+void vvodenie_v_stepen(int chislo, int stepen);
